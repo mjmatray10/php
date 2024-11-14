fd7f75d1ced1efb2e3a33e95f4f300e8d1b953b4
Mason Matray <mjmatray@icloud.com>
2024-11-13 19:37:38 -0600

    Update model-brands.php

--- model-brands.php
+++ model-brands.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectBrands() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT brand_id, brand_name, brand_origin FROM `brand`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
