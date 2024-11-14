882011cfb716cfdf03b9ddf7c4fe0ce6e6b69425
Mason Matray <mjmatray@icloud.com>
2024-11-13 19:53:32 -0600

    Update brands.php

--- brands.php
+++ brands.php
@@ -4,7 +4,7 @@ require_once("model-brands.php");
 
 $pageTitle = "Brands";
 include "view-header.php";
-$coaches = selectBrands();
+$brands = selectBrands();
 include "view-brands.php";
 include "view-footer.php";
 ?>
