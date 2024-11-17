0559468d1e20e21faa906b2ff22df463590b07b2
Mason Matray <mjmatray@icloud.com>
2024-11-17 00:02:17 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -74,6 +74,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="contact.php">Contact</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About Us</a>
+                </li>
             </ul>
         </div>
     </div>
