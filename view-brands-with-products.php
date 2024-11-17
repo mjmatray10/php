4437fdad7ccd791d54cde24d51158f10164902b1
Mason Matray <mjmatray@icloud.com>
2024-11-17 15:57:36 -0600

    Update view-brands-with-products.php

--- view-brands-with-products.php
+++ view-brands-with-products.php
@@ -19,7 +19,7 @@ while ($brand = $brands->fetch_assoc()) {
       <p class="card-text">
       <ul class="list-group">
       <?php
-        $products = selectBrandsByProduct($brand['brand_id']);
+        $products = selectBrandsByProducts($brand['brand_id']);
         while ($product = $products->fetch_assoc()) {
       ?>
         <li class="list-group-item">
