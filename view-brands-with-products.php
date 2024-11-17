649c6530c535efb77035af9e6df1306e033b31f7
Mason Matray <mjmatray@icloud.com>
2024-11-17 17:03:40 -0600

    Update view-brands-with-products.php

--- view-brands-with-products.php
+++ view-brands-with-products.php
@@ -31,7 +31,7 @@
                     <form method="post" action="" class="ms-2">
                       <input type="hidden" name="pid" value="<?php echo $product['product_id']; ?>">
                       <input type="hidden" name="actionType" value="Delete">
-                      <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?');" style="width: 40px; height: 40px; display: flex; align-items: center; justify-content: center;">
+                      <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?');" style="width: 30px; height: 30px; display: flex; align-items: center; justify-content: center;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-x" viewBox="0 0 16 16">
                           <path d="M6.854 7.146a.5.5 0 1 0-.708.708L7.293 9l-1.147 1.146a.5.5 0 0 0 .708.708L8 9.707l1.146 1.147a.5.5 0 0 0 .708-.708L8.707 9l1.147-1.146a.5.5 0 0 0-.708-.708L8 8.293z"/>
                           <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
