b6a18361ab4c94613c79d6db8f91dcc929e66860
Mason Matray <mjmatray@icloud.com>
2024-11-17 01:02:23 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -22,11 +22,11 @@
     </style>
     <div class="flat-under">
         <div class="shareon">
-            <a class="twitter"></a>
+            <a class="teams"></a>
             <a class="facebook"></a>
             <a class="linkedin"></a>
             <a class="reddit"></a>
-            <a class="pocket"></a>
+            <a class="copy-url"></a>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
