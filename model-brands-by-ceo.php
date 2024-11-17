31720d6d798f6af249d5e57f71cd44a2929e2504
Mason Matray <mjmatray@icloud.com>
2024-11-16 21:12:37 -0600

    Update model-brands-by-ceo.php

--- model-brands-by-ceo.php
+++ model-brands-by-ceo.php
@@ -2,7 +2,7 @@
 function selectCeoByBrands($bid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT b.brand_name, c.ceo_id, c.ceo_name, c.ceo_age FROM brand b JOIN ceo c ON b.brand_id = c.brand_id WHERE c.brand_id = ?");
+        $stmt = $conn->prepare("SELECT c.ceo_id, c.ceo_name, c.ceo_age FROM brand b JOIN ceo c ON b.brand_id = c.brand_id WHERE c.brand_id = ?");
         $stmt->bind_param("i", $bid);
         $stmt->execute();
         $result = $stmt->get_result();
