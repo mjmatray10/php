ddb2a23dfe3126d269c6c5e87accd06227b3dfea
Mason Matray <mjmatray@icloud.com>
2024-11-16 21:39:51 -0600

    Update dealers.php

--- dealers.php
+++ dealers.php
@@ -4,7 +4,7 @@ require_once("model-dealers.php");
 
 $pageTitle = "Dealer Record Table";
 include "view-header.php";
-$dealers = selectDealer();
+$dealers = selectDealers();
 include "view-dealers.php";
 include "view-footer.php";
 ?>
