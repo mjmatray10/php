027cf3c24a0e9a3e9a1c435dc8ade7791a92c4e5
Mason Matray <mjmatray@icloud.com>
2024-11-17 00:15:40 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -21,8 +21,16 @@
         color: #f8f9fa;
         box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
       }
+      .navbar-container {
+        max-width: 1200px;
+        margin: 20px auto; 
+        border-radius: 10px; 
+        overflow: hidden;
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+      }
       .navbar {
-        background-color: rgba(52, 58, 64, 0.8); /* Slight transparency */
+        background-color: rgba(52, 58, 64, 0.9);
+        border-radius: 0; 
       }
       .navbar-brand, .nav-link {
         color: #ffffff;
@@ -38,38 +46,37 @@
       .navbar-toggler-icon {
         background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3E%3Cpath stroke='rgba(255,255,255,0.7)' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
       }
-      .container-fluid {
-        max-width: 1200px;
-      }
     </style>
 </head>
-<nav class="navbar navbar-expand-lg">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Fishing Gear</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="brands.php">Brands</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="ceos.php">CEOs</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="dealers.php">Dealers</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.php">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="about.php">About Us</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<div class="navbar-container">
+  <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#">Fishing Gear</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                      <a class="nav-link" aria-current="page" href="/">Home</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="brands.php">Brands</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="ceos.php">CEOs</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="dealers.php">Dealers</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="contact.php">Contact</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="about.php">About Us</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+  </nav>
+</div>
