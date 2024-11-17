b6f7b561c29b7042d2ee324aa4847e4ab655bbff
Mason Matray <mjmatray@icloud.com>
2024-11-16 21:25:49 -0600

    Update brands-by-ceo.php

--- brands-by-ceo.php
+++ brands-by-ceo.php
@@ -4,7 +4,7 @@ require_once("model-brands-by-ceo.php");
 
 $pageTitle = "CEO of Brand";
 include "view-header.php";
-$universities = selectBrandsByCeos($_GET['id']);
+$ceos = selectBrandsByCeos($_GET['id']);
 include "view-brands-by-ceo.php";
 include "view-footer.php";
 ?>
