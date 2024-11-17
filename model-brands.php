e32008956ec094e09d283f0e23381f05cce645eb
Mason Matray <mjmatray@icloud.com>
2024-11-16 22:42:28 -0600

    Update model-brands.php

--- model-brands.php
+++ model-brands.php
@@ -12,4 +12,48 @@ function selectBrands() {
         throw $e;
     }
 }
+
+function insertBrands($bName, $bOrigin) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `brand` (`brand_name`, `brand_origin`) VALUES (?,?)");
+        $stmt->bind_param("ss", $bName, $bOrigin);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateBrands($bName, $bOrigin, $bid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `brand` set `brand_name` = ?, `brand_origin` = ? where brand_id = ?");
+        $stmt->bind_param("ssi", $bName, $bOrigin, $bid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteBrands($bid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from brand where brand_id = ?");
+        $stmt->bind_param("i", $bid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 ?>
