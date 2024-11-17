d646130c14f683f1f79d29b10714b1e10a0968ae
Mason Matray <mjmatray@icloud.com>
2024-11-17 01:01:14 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,4 +1,13 @@
     </div>
+    <link 
+      href="https://cdn.jsdelivr.net/npm/shareon@2/dist/shareon.min.css"
+      rel="stylesheet"
+    >
+    <script
+      src="https://cdn.jsdelivr.net/npm/shareon@2/dist/shareon.iife.js"
+      defer
+      init
+    ></script>
     <style>
       .flat-under {
         display: flex;
@@ -10,52 +19,14 @@
         bottom: 0;
         width: 100%;
       }
-      .grow-container {
-        display: inline-flex;
-      }
-      .circle-button {
-        display: inline-block;
-        width: 50px;
-        height: 50px;
-        border-radius: 50%;
-        text-align: center;
-        line-height: 50px;
-        transition: all 0.3s ease;
-        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
-      }
-      .circle-button:hover {
-        transform: scale(1.1);
-      }
-      .fa-fw {
-        font-size: 24px;
-        line-height: 50px;
-      }
     </style>
     <div class="flat-under">
-        <div class="grow-container">
-            <a href="#" class="to-twitter circle-button">
-                <i class="fa fa-twitter fa-fw" aria-hidden="true"></i>
-            </a>
-        </div>
-        <div class="grow-container">
-            <a href="#" class="to-red circle-button">
-                <i class="fa fa-google-plus fa-fw" aria-hidden="true"></i>
-            </a>
-        </div>
-        <div class="grow-container">
-            <a href="#" class="to-facebook circle-button">
-                <i class="fa fa-facebook fa-fw" aria-hidden="true"></i>
-            </a>
-        </div>
-        <div class="grow-container">
-            <a href="#" class="to-linkedin circle-button">
-                <i class="fa fa-linkedin fa-fw" aria-hidden="true"></i>
-            </a>
-        </div>
-        <div class="grow-container">
-            <a href="#" class="to-reddit circle-button">
-                <i class="fa fa-reddit-alien fa-fw" aria-hidden="true"></i>
-            </a>
+        <div class="shareon">
+            <a class="twitter"></a>
+            <a class="facebook"></a>
+            <a class="linkedin"></a>
+            <a class="reddit"></a>
+            <a class="pocket"></a>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
