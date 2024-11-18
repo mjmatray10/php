66a5f20c6b47d55233d8eccb18333434b41a621a
Mason Matray <mjmatray@icloud.com>
2024-11-17 18:28:58 -0600

    Create view-about.php

---
+++ view-about.php
@@ -0,0 +1,11 @@
+<?php
+$pageTitle = "About Us";
+?>
+<body>
+    <div class="content-container">
+        <h1>About Us</h1>
+    </br>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-vkFHwyxEd+fNItuOe6fEpGpdfH1A1Zgq3RRD+x4+2l6Cqf5rAxfjqPEH/hdbIWLn" crossorigin="anonymous"></script>
+</body>
