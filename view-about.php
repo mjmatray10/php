737a8f211fd89bf1aa3779ea0450ff41dfc75426
Mason Matray <mjmatray@icloud.com>
2024-11-17 18:35:06 -0600

    Update view-about.php

--- view-about.php
+++ view-about.php
@@ -4,8 +4,14 @@ $pageTitle = "About Us";
 <body>
     <div class="content-container">
         <h1>About Us</h1>
-    </br>
+        </br>
+        <video id="my-video" class="video-js vjs-default-skin" controls>
+            <source src="about-us.mp4" type="video/mp4">
+            Your browser does not support video.
+        </video>
     </div>
-    
+
+    <link href="https://vjs.zencdn.net/7.11.4/video-js.css" rel="stylesheet">
+    <script src="https://vjs.zencdn.net/7.11.4/video.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-vkFHwyxEd+fNItuOe6fEpGpdfH1A1Zgq3RRD+x4+2l6Cqf5rAxfjqPEH/hdbIWLn" crossorigin="anonymous"></script>
 </body>
